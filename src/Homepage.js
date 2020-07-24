81393b94be0f88937f55f5242a5766851ec99732
Sifon Isaac <syfonisaac@gmail.com>
2020-07-24 16:20:19 +0100

    homepage styling

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,38 +1,41 @@
 import React from 'react'
+import './Homepage.scss'
 
 const HomePage = () => {
   return (
-  <div className="homepage">
-    <div className="menu-item">
-      <div className="content">
-        <h1 className="title">Hats</h1>
-        <span className="subtitle">Shop Now</span>
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Hats</h1>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Shirts</h1>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Polo</h1>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Men</h1>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Women</h1>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
       </div>
-    </div>
-    <div className="menu-item">
-      <div className="content">
-        <h1 className="title">Shirts</h1>
-        <span className="subtitle">Shop Now</span>
-      </div>
-    </div>
-    <div className="menu-item">
-      <div className="content">
-        <h1 className="title">Polo</h1>
-        <span className="subtitle">Shop Now</span>
-      </div>
-    </div>
-    <div className="menu-item">
-      <div className="content">
-        <h1 className="title">Men</h1>
-        <span className="subtitle">Shop Now</span>
-      </div>
-    </div>
-    <div className="menu-item">
-      <div className="content">
-        <h1 className="title">Women</h1>
-        <span className="subtitle">Shop Now</span>
-      </div>
-    </div>
     </div>
   )
 }
