dcc57d594b673009859bf89f69d5ab243bd55b73
Sifon Isaac <syfonisaac@gmail.com>
2020-07-24 20:44:44 +0100

    components for homepage complete

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,41 +1,11 @@
 import React from 'react'
 import './Homepage.scss'
+import DirectoryMenu from '../../components/Directory/DirectoryMenu'
 
 const HomePage = () => {
   return (
     <div className="homepage">
-      <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Hats</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Shirts</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Polo</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Men</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Women</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-      </div>
+      <DirectoryMenu/>
     </div>
   )
 }
