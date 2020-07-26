a5a10dd0b213fabea6453c6ba7b2f9102fa7380e
Sifon Isaac <syfonisaac@gmail.com>
2020-07-26 22:27:21 +0100

    getting background image property

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -3,12 +3,12 @@ import './MenuItem.scss'
 
 const MenuItem = ({title, imageUrl, size}) => {
   return (
-    <div style={{
+    <div className={`${size} menu-item`}>
+      <div className="background-image" style={{
       backgroundImage : `url(${imageUrl})`
-    }}
-      className={`${size} menu-item`}>
+    }}/>
       <div className="content">
-        <h1 className="title">{title}</h1>
+        <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">Shop Now</span>
       </div>
     </div>
