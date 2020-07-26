90d739521d5e365cff7f4a566a70118220ed4b3b
Sifon Isaac <syfonisaac@gmail.com>
2020-07-26 23:52:58 +0100

    route setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Route} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/Homepage'
 
 function App() {
   return (
-    <HomePage/>
+    <Route exact path='/' component={HomePage}></Route>
   );
 }
 
