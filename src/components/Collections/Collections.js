23da4662a07a6bf0fafad42c222bbc4d8d6202a0
Sifon Isaac <syfonisaac@gmail.com>
2020-07-28 13:54:22 +0100

    collection item page

--- src/components/Collections/Collections.js
+++ src/components/Collections/Collections.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Collections.scss'
+import CollectionItem from './CollectionItem';
 function Collections({items,title}) {
   return (
     <div className="collection-preview">
       <h1 className="title"> {title.toUpperCase()}</h1>
       <div className="preview">
-        {items.filter((item, index)=>(index < 4)).map(item => (
-         <div key={item.id}> {item.name }</div>
+        {items.filter((item, index)=>(index < 4)).map(({id, ...OtherItemProps}) => (
+         <CollectionItem key={id} {...OtherItemProps } />
         ))}
-      </div>
       
+      </div>
     </div>
   );
 }
