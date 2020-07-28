570d77c9637f75ab29c26368de5691f5f2268479
Sifon Isaac <syfonisaac@gmail.com>
2020-07-28 09:05:08 +0100

    route trial

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import {Route} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/Homepage'
 
-const Hat = () => {
+const Hat = (props) => {
+  console.log(props)
   return (
     <h1>Hat Page</h1>
   )
@@ -12,7 +13,7 @@ function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage}></Route>
-      <Route exact path='/hats' component={Hat}></Route>
+      <Route exact path='/shop/hats' component={Hat}></Route>
     </div>
   );
 }
