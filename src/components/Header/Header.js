7f6e719f21afe5d18f785a18966d1f5c4e3e3219
Sifon Isaac <syfonisaac@gmail.com>
2020-07-31 16:21:40 +0100

    header style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
+import './Header.scss'
 
 function Header() {
   return (
   <div className="header">
-    <Link className="logo-container" to="/ ">
+    <Link className="logo-container" to="/">
       <Logo className="logo"/>
     </Link>
     <div className="options">
-      <Link className="option">
+      <Link className="option" to="/shop">
         Shop
       </Link>
       <Link className="option">
