5ddd3802a4559bfb8b91f8a02fa850b8be8fdc1f
Sifon Isaac <syfonisaac@gmail.com>
2020-08-02 15:41:07 +0100

    login form logic

---
+++ src/components/SignIn/SignIn.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import './SignIn.scss'
+
+class SignIn extends React.Component{
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      password: ''
+    }
+
+    handleSubmit = event => {
+      event.preventDefault();
+      this.setState({
+        email: '',
+        password: ''
+      })
+    }
+    handleChange = event => {
+      const {name , value }= event.preventDefault();
+      this.setState({[name]:value})
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>I already have an account</h2>
+        <span>Sign In with your email and password</span>
+        <form onSubmit={this.handleSubmit}>
+          <input name="email" type="email" value={this.state.email} required />
+          <label>Email</label>
+          <input name="password" type="password" value={this.state.password}  required />
+          <label>Password</label>
+
+          <input type="submit" value="Submit Form" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SignIn
