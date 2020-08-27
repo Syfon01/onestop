913154a987de27b114bcb73bc90c17702cd16342
Sifon Isaac <syfonisaac@gmail.com>
2020-08-27 16:02:55 +0100

    styling for auth page

--- src/pages/auth/auth.js
+++ src/pages/auth/auth.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import SignIn from '../../components/SignIn/SignIn'
 import SignUp from '../../components/SignUp/sign-up'
-
+import './auth.scss'
 function Auth() {
   return (
     <div className="auth">
       <SignIn />
-      <SignUp/>
+      <SignUp />
     </div>
   )
 }
